69f142cfc23c3f8c023d6a122e7ca0b6dbe2c8f9
Gazzaev Yaroslav Eduardovich <iegazzaev@edu.hse.ru>
2024-05-25 13:52:13 +0300

    with graphic report

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QLabel, QTableView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QLabel, QTableView, QWidget
 import pandas as pd
+
+import matplotlib.pyplot as plt
 from PyQt5.QtCore import QAbstractTableModel, Qt
 
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 BOOKS = pd.read_excel('./data.xlsx')
 
 class MyWidget(QMainWindow):
@@ -22,6 +26,7 @@ class MyWidget(QMainWindow):
     def show_dataset_view(self):
         self.dataset_window = DatasetView(BOOKS)
         self.dataset_window.show()
+
     def open_graphic_report(self):
         # Логика для открытия отчета или другого окна
         print("Graphic report button clicked")
@@ -69,7 +74,68 @@ class DatasetView(QDialog):
 class GraphicReportView(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('graphic_report.ui', self)
+        uic.loadUi('graphic_reports.ui', self)
+        self.kach_kach.clicked.connect(self.kach_kach_clicked)
+
+    def kach_kach_clicked(self):
+        self.kach_kach_window = GraphicsReport_kach_kach()
+        self.kach_kach_window.show()
+
+
+class GraphicsReport_kach_kach(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('kach_kach_window.ui', self)
+        self.bulid_btn.clicked.connect(self.bulid_btn_clicked)
+
+    def bulid_btn_clicked(self):
+        selected_text = self.comboBox.currentText().split(' - ')
+        first_param, second_param = selected_text[0], selected_text[1]
+
+        self.build_window = GraphicsReport_kach_kach_View(first_param, second_param)
+        self.build_window.show()
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class GraphicsReport_kach_kach_View(QDialog):
+    def __init__(self, first_param, second_param):
+        super().__init__()
+        self.first_param = first_param
+        self.second_param = second_param
+
+
+        self.setWindowTitle('Качественный-Качественный')
+        self.setGeometry(100, 100, 800, 600)
+
+        sc = MplCanvas(self, width=5, height=4, dpi=100)
+
+        # Пример данных и создание DataFrame
+        data = {
+            self.first_param: BOOKS[self.first_param],
+            self.second_param: BOOKS[self.second_param]
+        }
+        df = pd.DataFrame(data)
+        # Исключение данных по стране "США"
+        df = df[df[self.first_param] != 'USA']
+        df = df[df[self.second_param] != 'USA']
+        count_data = df.groupby([self.first_param, self.second_param]).size().unstack()
+
+        # Создание кластеризованной столбчатой диаграммы
+        count_data.plot(kind='bar', stacked=False, ax=sc.axes)
+        sc.axes.set_xlabel(self.first_param)
+        sc.axes.set_ylabel(self.second_param)
+        sc.axes.set_title(f'Clustered Bar Chart for {self.first_param} and {self.second_param}')
+        sc.axes.legend(title=self.second_param)
+
+        layout = QVBoxLayout()
+        layout.addWidget(sc)
+
+        self.setLayout(layout)
 
 
 
