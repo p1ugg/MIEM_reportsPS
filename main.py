ecb392d29a4eb1b6318080638b46f7b5b79bda05
Gazzaev Yaroslav Eduardovich <iegazzaev@edu.hse.ru>
2024-06-11 09:42:06 +0300

    full code \ no bug fixed with graphics with Price

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QLabel, QTableView, QWidget, QTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout, QLabel, QTableView, QWidget, \
+    QTextEdit
 import pandas as pd
+import seaborn as sns
 
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import QAbstractTableModel, Qt
@@ -60,7 +62,6 @@ class TextReportsView(QDialog):
         self.text_report1_view = TextReportView12(1)
         self.text_report1_view.show()
 
-
     def tr2_clicked(self):
         self.text_report2_view = TextReportView12(2)
         self.text_report2_view.show()
@@ -84,14 +85,19 @@ class TextReportView12(QDialog):
     def bulid_btn_clicked(self):
         # Настройка QTableView
         N = self.nLine.toPlainText()
-        print(N)
-        self.text_report_view = self.findChild(QTableView, 'text_report_view')
-        if self.num == 1:
-            self.df = get_most_expensive_books(BOOKS, int(N))
-        elif self.num == 2:
-            self.df = get_most_discussed_authors(BOOKS,  int(N))
-        self.model = PandasModel(self.df)
-        self.text_report_view.setModel(self.model)
+        try:
+            if 2 <= int(N) <= 550:
+                self.text_report_view = self.findChild(QTableView, 'text_report_view')
+                if self.num == 1:
+                    self.df = get_most_expensive_books(BOOKS, int(N))
+                elif self.num == 2:
+                    self.df = get_most_discussed_authors(BOOKS, int(N))
+                self.model = PandasModel(self.df)
+                self.text_report_view.setModel(self.model)
+
+        except Exception as ex:
+            pass
+
 
 class TextReportView34(QDialog):
     def __init__(self, df):
@@ -101,12 +107,10 @@ class TextReportView34(QDialog):
         self.df = df
 
     def bulid_btn_clicked(self):
-
         # Настройка QTableView
         self.text_report_view = self.findChild(QTableView, 'text_report_view')
         self.model = PandasModel(self.df)
         self.text_report_view.setModel(self.model)
-
 
 
 class PandasModel(QAbstractTableModel):
@@ -152,7 +156,8 @@ class GraphicReportView(QDialog):
         uic.loadUi('graphic_reports.ui', self)
         self.kach_kach.clicked.connect(self.kach_kach_clicked)
         self.kol_kach.clicked.connect(self.kol_kach_clicked)
-
+        self.baw_kol_kach.clicked.connect(self.baw_kol_kach_clicked)
+        self.twokol_kach.clicked.connect(self.twokol_kach_clicked)
 
     def kach_kach_clicked(self):
         self.kach_kach_window = GraphicsReport_kach_kach()
@@ -161,6 +166,118 @@ class GraphicReportView(QDialog):
     def kol_kach_clicked(self):
         self.kol_kach_window = GraphicsReport_kol_kach()
         self.kol_kach_window.show()
+
+    def baw_kol_kach_clicked(self):
+        self.baw_kol_kach_window = GraphicsReport_baw_kol_kach()
+        self.baw_kol_kach_window.show()
+
+    def twokol_kach_clicked(self):
+        self.twokol_kach_window = GraphicsReport_twokol_kach()
+        self.twokol_kach_window.show()
+
+
+class GraphicsReport_twokol_kach(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('2kol_kach_window.ui', self)
+        self.bulid_btn.clicked.connect(self.bulid_btn_clicked)
+
+    def bulid_btn_clicked(self):
+        selected_text = self.comboBox.currentText().split(' - ')
+        first_param, second_param, third_param = selected_text[0], selected_text[1], selected_text[2]
+        print(first_param, second_param, third_param)
+        self.build_window = GraphicsReport_twokol_kach_View(first_param, second_param, third_param)
+        self.build_window.show()
+
+
+# Класс для отображения графика
+class GraphicsReport_twokol_kach_View(QWidget):
+    def __init__(self, first_param, second_param, third_param, parent=None):
+        super().__init__(parent)
+        self.first_param = first_param
+        self.second_param = second_param
+        self.third_param = third_param
+        self.initUI()
+
+    def initUI(self):
+        self.layout = QVBoxLayout(self)
+
+        # Создаем холст (canvas)
+        self.canvas = MplCanvas(self)
+        self.layout.addWidget(self.canvas)
+
+        # Строим график
+        self.plot()
+
+    def plot(self):
+        self.canvas.axes.clear()
+        data = {
+            self.first_param: BOOKS[self.first_param],
+            self.second_param: BOOKS[self.second_param],
+            self.third_param: BOOKS[self.third_param]
+        }
+
+        df = pd.DataFrame(data)
+
+        # Построение диаграммы рассеивания
+        sns.scatterplot(x=self.first_param, y=self.second_param, hue=self.third_param, data=df, ax=self.canvas.axes)
+        self.canvas.axes.set_xlabel(self.first_param)
+        self.canvas.axes.set_ylabel(self.second_param)
+        self.canvas.axes.set_title(f'Categorized Scatter Plot for Numeric Attributes by {self.third_param}')
+
+        # Обновление холста
+        self.canvas.draw()
+
+
+class GraphicsReport_baw_kol_kach(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('kol_kach_baw_window.ui', self)
+        self.bulid_btn.clicked.connect(self.bulid_btn_clicked)
+
+    def bulid_btn_clicked(self):
+        selected_text = self.comboBox.currentText().split(' - ')
+        first_param, second_param = selected_text[0], selected_text[1]
+        print(first_param, second_param)
+        self.build_window = GraphicsReport_baw_kol_kach_View(first_param, second_param)
+        self.build_window.show()
+
+
+# Класс для отображения графика
+class GraphicsReport_baw_kol_kach_View(QWidget):
+    def __init__(self, first_param, second_param, parent=None):
+        super().__init__(parent)
+        self.first_param = first_param
+        self.second_param = second_param
+        self.initUI()
+
+    def initUI(self):
+        self.layout = QVBoxLayout(self)
+
+        # Создаем холст (canvas)
+        self.canvas = MplCanvas(self)
+        self.layout.addWidget(self.canvas)
+
+        # Строим график
+        self.plot()
+
+    def plot(self):
+        self.canvas.axes.clear()
+        data = {
+            self.first_param: BOOKS[self.first_param],
+            self.second_param: BOOKS[self.second_param]
+        }
+
+        df = pd.DataFrame(data)
+        # Построение боксплота
+        df.boxplot(column=self.first_param, by=self.second_param, vert=False, ax=self.canvas.axes)
+        self.canvas.axes.set_xlabel(self.first_param)
+        self.canvas.axes.set_ylabel(self.second_param)
+        self.canvas.axes.set_title(
+            f'Categorized Box-and-Whiskers Diagram for {self.first_param} by {self.second_param}')
+
+        # Обновление холста
+        self.canvas.draw()
 
 
 class GraphicsReport_kach_kach(QDialog):
@@ -172,7 +289,6 @@ class GraphicsReport_kach_kach(QDialog):
     def bulid_btn_clicked(self):
         selected_text = self.comboBox.currentText().split(' - ')
         first_param, second_param = selected_text[0], selected_text[1]
-
         self.build_window = GraphicsReport_kach_kach_View(first_param, second_param)
         self.build_window.show()
 
@@ -219,7 +335,6 @@ class GraphicsReport_kach_kach_View(QDialog):
         self.setLayout(layout)
 
 
-
 class GraphicsReport_kol_kach(QDialog):
     def __init__(self):
         super().__init__()
@@ -232,6 +347,7 @@ class GraphicsReport_kol_kach(QDialog):
 
         self.build_window = GraphicsReport_kol_kach_View(first_param, second_param)
         self.build_window.show()
+
 
 class GraphicsReport_kol_kach_View(QDialog):
     def __init__(self, first_param, second_param):
@@ -266,6 +382,7 @@ class GraphicsReport_kol_kach_View(QDialog):
         layout = QVBoxLayout()
         layout.addWidget(sc)
         self.setLayout(layout)
+
 
 # class GraphicsReport_kol_kach_View(QDialog):
 #     def __init__(self, first_param, second_param):
